cb77fba64921d4791b3dffcf6cf2f146a39d844c
kfujita <kfujita@student.42tokyo.jp>
2023-02-13 09:48:16 +0900

    定数を`define`化した
    `Global variables are forbidden`なため、念の為。

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,13 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 21:40:11 by kfujita           #+#    #+#             */
-/*   Updated: 2023/02/12 23:33:49 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/02/13 09:11:54 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/stack.h"
+
+#include "../libft/ft_put/ft_put.h"
 
 // - EXIT_FAILURE
 // - EXIT_SUCCESS
@@ -24,7 +26,7 @@
 // - true / false
 #include <stdbool.h>
 
-static const char	g_error_msg[] = "Error\n";
+#define ERROR_MSG ("Error\n")
 
 static bool	parse_arg(int argc, char *const *argv, t_stacks *stacks)
 {
@@ -32,12 +34,12 @@ static bool	parse_arg(int argc, char *const *argv, t_stacks *stacks)
 		return (false);
 	if (argc != 2 || argv == NULL || argv[1] == NULL || argv[1][0] == '\0')
 	{
-		write(STDERR_FILENO, g_error_msg, sizeof(g_error_msg));
+		ft_putstr_fd(ERROR_MSG, STDERR_FILENO);
 		return (false);
 	}
 	if (!init_stack_from_str(argv[1], stacks))
 	{
-		write(STDERR_FILENO, g_error_msg, sizeof(g_error_msg));
+		ft_putstr_fd(ERROR_MSG, STDERR_FILENO);
 		return (false);
 	}
 	return (true);
