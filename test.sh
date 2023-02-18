b7104c1ed073691b4c58bf3170d81b324fc52c0b
kfujita <kfujita@student.42tokyo.jp>
2023-02-19 04:55:06 +0900

    len=6までをテスト

--- test.sh
+++ test.sh
@@ -19,3 +19,18 @@ pusw '2 1 3'
 pusw '2 3 1'
 pusw '3 2 1'
 pusw '3 1 2'
+
+pusw '1 2 3 4'
+pusw '1 2 4 3'
+pusw '2 1 3 4'
+pusw '3 1 4 2'
+pusw '4 3 2 1'
+pusw '4 2 1 3'
+
+pusw '5 4 3 2 1'
+pusw '5 1 4 2 3'
+pusw '3 1 5 4 2'
+
+pusw '6 5 4 3 2 1'
+pusw '5 2 1 3 4 6'
+pusw '2 6 3 4 5 1'
