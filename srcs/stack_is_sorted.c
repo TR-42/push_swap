ef9640b46cd952fcfd59ac708a8d6823d87b738a
kfujita <kfujita@student.42tokyo.jp>
2023-03-10 17:06:34 +0900

    `is_sorted`関数を追加し、それを活用する形に書き直した

--- srcs/stack_is_sorted.c
+++ srcs/stack_is_sorted.c
@@ -6,44 +6,47 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 01:17:52 by kfujita           #+#    #+#             */
-/*   Updated: 2023/02/19 05:39:55 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/03/10 17:19:33 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/stack.h"
 
+static bool	is_sorted(int *arr, size_t len)
+{
+	size_t	i;
+	int		last_value;
+
+	if (arr == NULL || len == 0)
+		return (false);
+	i = 0;
+	last_value = *(arr++);
+	while (++i < len)
+	{
+		if (*arr < last_value)
+			return (false);
+		last_value = *arr;
+		arr++;
+	}
+	return (true);
+}
+
 bool	is_a_sorted(t_stacks *s)
 {
 	size_t	i;
 
-	if (s == NULL || s->a == NULL || s->a_len <= 0)
+	if (s == NULL)
 		return (false);
-	if (s->a_len == 1)
-		return (true);
-	i = 0;
-	while (++i < s->a_len)
-	{
-		if (s->a[i] < s->a[i - 1])
-			return (false);
-	}
-	return (true);
+	return (is_sorted(s->a, s->a_len));
 }
 
 bool	is_b_sorted(t_stacks *s)
 {
 	size_t	i;
 
-	if (s == NULL || s->b == NULL || s->b_len <= 0)
+	if (s == NULL)
 		return (false);
-	if (s->b_len == 1)
-		return (true);
-	i = 0;
-	while (++i < s->b_len)
-	{
-		if (s->b[i] > s->b[i - 1])
-			return (false);
-	}
-	return (true);
+	return (is_sorted(s->b, s->b_len));
 }
 
 bool	stack_is_sorted_a_b(t_stacks *s)
@@ -68,5 +71,6 @@ bool	stack_is_sorted_a_b(t_stacks *s)
 
 bool	stack_is_sorted(t_stacks *stacks)
 {
-	return (stacks != NULL && stacks->b_len <= 0 && is_a_sorted(stacks));
+	return (stacks != NULL && stacks->b_len <= 0
+		&& is_sorted(stacks->a, stacks->a_len));
 }
