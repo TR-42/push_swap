e2e604ef8af173bfecc5977ce59dbf8d3a5c50cb
kfujita <kfujita@student.42tokyo.jp>
2023-03-16 10:03:34 +0900

    `_stack_split`を`stack_sort_qsort`に展開

--- srcs/stack_sort_qsort.c
+++ srcs/stack_sort_qsort.c
@@ -6,7 +6,7 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 04:57:31 by kfujita           #+#    #+#             */
-/*   Updated: 2023/03/16 09:58:47 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/03/16 10:03:13 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,29 +53,27 @@ static void	_stack_split_push_to_a(t_stacks *s,
 		_sort_and_pa(s, len, min, log_vect);
 }
 
-static void	_stack_split(t_stacks *s, size_t len, int min, t_vect *log_vect)
+bool	stack_sort_qsort(t_stacks *s, t_vect *log_vect)
 {
+	size_t	len;
+
+	len = s->a_len;
 	while (s->b_len != (len / 2))
 	{
-		if (len <= 5 || s->a[0] < (min + (int)(len / 2)))
+		if (len <= 5 || s->a[0] < (INT_MIN + (int)(len / 2)))
 			push_b(s, log_vect);
 		else
 			rotate_a(s, log_vect);
 	}
 	if (5 < len)
 	{
-		_stack_split_push_to_a(s, (len / 2), min, log_vect);
+		_stack_split_push_to_a(s, (len / 2), INT_MIN, log_vect);
 		while (s->b_len != (len - (len / 2)))
 			push_b(s, log_vect);
 		_stack_split_push_to_a(s,
-			len - (len / 2), min + (int)(len / 2), log_vect);
+			len - (len / 2), INT_MIN + (int)(len / 2), log_vect);
 	}
 	else
-		_sort_and_pa(s, len, min, log_vect);
-}
-
-bool	stack_sort_qsort(t_stacks *s, t_vect *log_vect)
-{
-	_stack_split(s, s->a_len, INT_MIN, log_vect);
+		_sort_and_pa(s, len, INT_MIN, log_vect);
 	return (true);
 }
